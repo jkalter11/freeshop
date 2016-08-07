b1a95941d61d171cd2baec4b5fd19ad38be31541
Thomas Edvalson <machin3@gmail.com>
2016-08-07 18:20:47 -0400

    Add ScrollBar/Scrollable

---
+++ src/GUI/Scrollable.cpp
@@ -0,0 +1,30 @@
+#include "Scrollable.hpp"
+#include "ScrollBar.hpp"
+
+namespace FreeShop {
+
+Scrollable::Scrollable()
+: m_scrollBar(nullptr)
+, m_scrollVal(0.f)
+{
+
+}
+
+Scrollable::~Scrollable()
+{
+	if (m_scrollBar)
+		m_scrollBar->detachObject(this);
+}
+
+void Scrollable::attachScrollbar(ScrollBar *scrollbar)
+{
+	m_scrollBar = scrollbar;
+}
+
+void Scrollable::detachScrollbar()
+{
+	m_scrollBar = nullptr;
+}
+
+
+} // namespace FreeShop
---
+++ src/GUI/Scrollable.hpp
@@ -0,0 +1,30 @@
+#ifndef FREESHOP_SCROLLABLE_HPP
+#define FREESHOP_SCROLLABLE_HPP
+
+#include <cpp3ds/System/Vector2.hpp>
+
+namespace FreeShop {
+
+class ScrollBar;
+
+class Scrollable {
+friend class ScrollBar;
+public:
+	Scrollable();
+	~Scrollable();
+	virtual void setScroll(float position) = 0;
+	virtual float getScroll() = 0;
+	virtual const cpp3ds::Vector2f &getSize() = 0;
+
+protected:
+	void attachScrollbar(ScrollBar *scrollbar);
+	void detachScrollbar();
+
+private:
+	float m_scrollVal;
+	ScrollBar *m_scrollBar;
+};
+
+} // namespace FreeShop
+
+#endif // FREESHOP_SCROLLABLE_HPP
