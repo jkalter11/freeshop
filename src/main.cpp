0d9852a156d393adfaff43544c7a9fd242125803
arc13 <alexandresollier@gmail.com>
2017-05-26 20:28:54 +0200

    Games on cartridges can now be launched

--- src/main.cpp
+++ src/main.cpp
@@ -79,14 +79,47 @@ int main(int argc, char** argv)
 	newsExit();
 	
 	if (FreeShop::g_requestShutdown) {
+		//Init the services and turn off the console
 		ptmSysmInit();
 		PTMSYSM_ShutdownAsync(0);
 		ptmSysmExit();
 	} else if (FreeShop::g_requestJump != 0) {
+		//Var initialization
 		Result res = 0;
 		u8 hmac[0x20];
 		memset(hmac, 0, sizeof(hmac));
+		
+		//Check on which media launch the title
 		FS_MediaType mediaType = ((FreeShop::g_requestJump >> 32) == FreeShop::TitleKeys::DSiWare) ? MEDIATYPE_NAND : MEDIATYPE_SD;
+		
+		FS_CardType type;
+		bool cardInserted;
+		cardInserted = (R_SUCCEEDED(FSUSER_CardSlotIsInserted(&cardInserted)) && cardInserted && R_SUCCEEDED(FSUSER_GetCardType(&type)) && type == CARD_CTR);
+		if (cardInserted)
+		{
+			// Retry a bunch of times. When the card is newly inserted,
+			// it sometimes takes a short while before title can be read.
+			int retryCount = 100;
+			u64 cardTitleId;
+			while (retryCount-- > 0)
+				if (R_SUCCEEDED(AM_GetTitleList(nullptr, MEDIATYPE_GAME_CARD, 1, &cardTitleId)))
+				{
+					try
+					{
+						if (cardTitleId == FreeShop::g_requestJump)
+							mediaType = MEDIATYPE_GAME_CARD;
+					}
+					catch (int e)
+					{
+						//
+					}
+					break;
+				}
+				else
+					cpp3ds::sleep(cpp3ds::milliseconds(5));
+		}
+		
+		//Do the application jump
 		if (R_SUCCEEDED(res = APT_PrepareToDoApplicationJump(0, FreeShop::g_requestJump, mediaType)))
 			res = APT_DoApplicationJump(0, 0, hmac);
 	}
